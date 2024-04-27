ada1a71423a9f8a63efd37e2460c7530d6faaaca
PoojaDonuru <donurupooja@gmail.com>
2024-04-27 14:46:38 -0400

    first commit

---
+++ pooja_vue/src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="app-footer">
+      <p>&copy; 2024 Online Ticketing System. All rights reserved.</p>
+    </footer>
+</template>
+  
+<style>
+ .app-footer {
+  background-color: #343a40;
+  color: #ffffff;
+  padding: 20px;
+  text-align: center;
+  width: 100%;
+}
+</style>
+  
